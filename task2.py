83b511623a6e89cfbe51ada03799d2eb041c32e7
Ashley Berna <arbgab2020@gmail.com>
2026-06-17 20:24:45 +0000

    task 2 is done

--- task2.py
+++ task2.py
@@ -7,8 +7,8 @@ def load_data(filename):
         reader = csv.reader(f)
         next(reader)
         for row in reader:
-            xs.append(float(row[0]))
-            ys.append(float(row[3]))
+            xs.append(float(row[1]))
+            ys.append(float(row[2]) / 1000)
     return xs, ys
 
 def predict(x, w0, w1):
@@ -21,7 +21,7 @@ def cost(xs, ys, w0, w1):
         total += (y_pred - ys[i]) ** 2
     return total / len(xs)
 
-def train(xs, ys, learningRate = 0.00000000001, iterations = 5000):
+def train(xs, ys, learningRate = 0.01, iterations = 5000):
     w0 = 0
     w1 = 0
 
@@ -32,8 +32,8 @@ def train(xs, ys, learningRate = 0.00000000001, iterations = 5000):
         for j in range(len(xs)):
             y_pred = predict(xs[j], w0, w1)
             error = y_pred - ys[j]
-            d_w0 += error
-            d_w1 += error * xs[j]
+            d_w0 += error / len(xs)
+            d_w1 += error * xs[j] / len(xs)
 
         w0 -= learningRate * d_w0
         w1 -= learningRate * d_w1
@@ -43,7 +43,7 @@ def train(xs, ys, learningRate = 0.00000000001, iterations = 5000):
 
     return w0, w1
 
-xs, ys = load_data("house_data.csv")
+xs, ys = load_data("Salary_dataset.csv")
 
 print("Training model...")
 w0, w1 = train(xs, ys)
@@ -51,6 +51,6 @@ w0, w1 = train(xs, ys)
 print("\nTraining complete!")
 print("Final weights:", w0, w1)
 
-new_x = float(input("\nEnter a house size (sq_feet) to predict price: "))
-prediction = predict(new_x, w0, w1)
+new_x = float(input("\nEnter years of experience to predict your salary: "))
+prediction = predict(new_x, w0, w1) * 1000
 print(f"Predicted price: ${prediction:,.2f}")
